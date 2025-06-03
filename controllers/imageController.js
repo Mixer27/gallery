31952e8c9ed60d01a5061cc80d96b7a61993d913
Mix <mikikorolczyk@gmail.com>
2025-06-03 11:40:59 +0200

    feat: buttons in gallery, show single image

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,6 +1,7 @@
 const image = require("../models/image");
 const gallery = require("../models/gallery");
 const asyncHandler = require("express-async-handler");
+const user = require("../models/user");
 
 exports.image_list = asyncHandler(async (req, res, next) => {
   const all_images = await image.find({}).populate("gallery").exec();
@@ -73,3 +74,103 @@ exports.image_add_post = [
     });
   })
 ];
+
+// IMAGE UPDATE GET
+// Kontroler wyświetlania formularza update'u obrazka - GET.
+exports.image_update_get = asyncHandler(async (req, res, next) => {
+let all_galleries;
+if (req.user.username === "admin") {
+// dane do formularza - zalogowany admin
+all_galleries = await gallery.find({}).sort({ name: 1 }).exec();
+} else {
+// dane do formularza - użytkownik zwykły
+let user_id = await user.findOne({ "username": req.user.username }).exec();
+all_galleries = await gallery.find({ "user": user_id }).exec();
+}
+//const image_id = req.query.image_id;
+const imageObj = await image.findOne({ _id: req.query.image_id }).exec();
+res.render("image_update", {
+title: "Image update:",
+image: imageObj,
+galleries: all_galleries,
+messages: []
+});
+});
+
+// IMAGE SHOW GET
+// Kontroler wyświetlania powiększonego obrazka
+exports.image_show_get = asyncHandler(async (req, res, next) => {
+  const image_id = req.query.image_id;
+
+  try {
+    const img = await image.findById(image_id).populate("gallery").exec();
+
+    if (!img) {
+      return res.status(404).render("info", { title: "Image not found", messages: ["Image not found."] });
+    }
+
+    res.render("image_show", {
+      title: `Image: ${img.name}`,
+      image: img,
+    });
+  } catch (err) {
+    next(err); // Pass to default error handler
+  }
+});
+
+// IMAGE UPDATE POST
+// Kontroler przetwarzania formularza update'u obrazka - POST.
+exports.image_update_post = asyncHandler(async (req, res, next) => {
+const filter = {_id: req.query.image_id};
+const update = {
+name: req.body.i_name,
+description: req.body.i_description,
+gallery: req.body.i_gallery,
+};
+let doc=await image.findOneAndUpdate(filter, update);
+if (doc) {
+res.redirect("../galleries/gallery_browse")
+} else {
+res.send('Image update error');
+}
+});
+
+// IMAGE DELETE
+// Kontroler usuwania obrazka - POST
+exports.image_delete = asyncHandler(async (req, res, next) => {
+const filter = {_id: req.params.image_id};
+let doc=await image.findOneAndDelete(filter);
+if (doc) {
+res.redirect("../../galleries/gallery_browse")
+} else {
+res.send('Error - image not deleted');
+}
+});
+
+// UPLOAD GET
+// Formularz wyboru pliku uploadu
+exports.image_upload_get = asyncHandler(async (req, res, next) => {
+res.render("image_upload_form", { title: "Upload image:" });
+});
+
+// UPLOAD POST
+// Obsługa danych formularza uploadu
+const formidable = require('formidable');
+const path = require("path");
+exports.image_upload_post = asyncHandler(async (req, res, next) => {
+const form = new formidable.IncomingForm({
+uploadDir: path.join(__dirname, "../public/images"),
+multiples: false,
+keepExtensions: true
+});
+let messages = [];
+form.parse(req, (err, fields, files) => {
+if (err) {
+messages.push("Image upload error!");
+}
+else {
+messages.push("Image uploaded!");
+}
+res.render("image_upload_form", { title: "Upload image:", messages });
+});
+});
--- routes/images.js
+++ routes/images.js
@@ -6,9 +6,26 @@ const image_controller = require("../controllers/imageController");
 
 router.get("/", image_controller.image_list);
 
+// Wyświetlania powiększonego obrazka
+router.get("/image_show", image_controller.image_show_get);
+
 //Wyświetlanie formularza dodawania obrazka - GET.
 router.get("/image_add", authenticate, image_controller.image_add_get);
 //Przetwarzanie danych formularza dodawania obrazka - POST.
 router.post("/image_add", authenticate, image_controller.image_add_post);
+
+//Update obrazka - GET.
+router.get("/image_update", authenticate, image_controller.image_update_get);
+//Update obrazka - POST
+router.post("/image_update", authenticate, image_controller.image_update_post);
+
+//Delete obrazka - DEL.
+router.post("/image_delete/:image_id", authenticate,
+image_controller.image_delete);
+
+//Wyświetlanie formularza uploadu obrazka - GET.
+router.get("/image_upload", authenticate, image_controller.image_upload_get);
+//Przetwarzanie danych formularza uploadu obrazka - POST.
+router.post("/image_upload", authenticate, image_controller.image_upload_post);
 
 module.exports = router;
